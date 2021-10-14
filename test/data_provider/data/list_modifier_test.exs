5bc332dfd5eb9a4e7120029c306273d0e55a3808
Shakarim Sapa <info@relitech.kz>
2021-10-14 23:48:07 +0600

    full rework `DataProvider.Data.ListModifierTest` and `DataProvider.Data.QueryModifierTest`

--- test/data_provider/data/list_modifier_test.exs
+++ test/data_provider/data/list_modifier_test.exs
@@ -3,89 +3,143 @@ defmodule DataProvider.Data.ListModifierTest do
   alias DataProvider.Data.ListModifier
   doctest DataProvider.Data.ListModifier
 
-  def fixture(:data_provider, _params), do: DataProvider.create()
   def fixture(:data_list, _params), do: Enum.to_list(1..1000)
-
-  defp data_provider(params), do: {:ok, data_provider: fixture(:data_provider, params)}
 
   defp data_list(params), do: {:ok, data_list: fixture(:data_list, params)}
 
-  defp load_state(data_provider, page_size, page),
-       do: %{data_provider | pagination: %{data_provider.pagination | page_size: page_size, page: page}}
-
-  setup [:data_provider, :data_list]
+  setup [:data_list]
 
   describe "`modify/2` empty list |" do
-    test "test result (page size: 0, page: 0)", %{data_provider: data_provider} do
-      data_provider = load_state(data_provider, 0, 0)
+    test "test result (page: 0, page size: 0)" do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
 
       assert ListModifier.modify([], data_provider) === []
     end
 
-    test "test result (page size: 0, page: 1)", %{data_provider: data_provider} do
-      data_provider = load_state(data_provider, 0, 1)
+    test "test result (page: 1, page size: 0)" do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 1,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
 
       assert ListModifier.modify([], data_provider) === []
     end
 
-    test "test result (page size: 0, page: 13)", %{data_provider: data_provider} do
-      data_provider = load_state(data_provider, 0, 13)
+    test "test result (page: 13, page size: 0)" do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 13,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
 
       assert ListModifier.modify([], data_provider) === []
     end
 
-    test "test result (page size: 13, page: 0)", %{data_provider: data_provider} do
-      data_provider = load_state(data_provider, 13, 0)
+    test "test result (page: 0, page size: 13)" do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
 
       assert ListModifier.modify([], data_provider) === []
     end
 
-    test "test result (page size: 13, page: 1)", %{data_provider: data_provider} do
-      data_provider = load_state(data_provider, 13, 1)
+    test "test result (page: 1, page size: 13)" do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 1,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
 
       assert ListModifier.modify([], data_provider) === []
     end
 
-    test "test result (page size: 13, page: 13)", %{data_provider: data_provider} do
-      data_provider = load_state(data_provider, 13, 13)
+    test "test result (page: 13, page size: 13)" do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 13,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
 
       assert ListModifier.modify([], data_provider) === []
     end
   end
 
   describe "`modify/2` filled list |" do
-    test "test result (page size: 0, page: 0)", %{data_list: data_list, data_provider: data_provider} do
-      data_provider = load_state(data_provider, 0, 0)
+    test "test result (page: 0, page size: 0)", %{data_list: data_list} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
 
       assert ListModifier.modify(data_list, data_provider) === []
     end
 
-    test "test result (page size: 0, page: 1)", %{data_list: data_list, data_provider: data_provider} do
-      data_provider = load_state(data_provider, 0, 1)
+    test "test result (page: 1, page size: 0)", %{data_list: data_list} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 1,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
 
       assert ListModifier.modify(data_list, data_provider) === []
     end
 
-    test "test result (page size: 0, page: 13)", %{data_list: data_list, data_provider: data_provider} do
-      data_provider = load_state(data_provider, 0, 13)
+    test "test result (page: 13, page size: 0)", %{data_list: data_list} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 13,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
 
       assert ListModifier.modify(data_list, data_provider) === []
     end
 
-    test "test result (page size: 13, page: 0)", %{data_list: data_list, data_provider: data_provider} do
-      data_provider = load_state(data_provider, 13, 0)
+    test "test result (page: 0, page size: 13)", %{data_list: data_list} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
 
       assert ListModifier.modify(data_list, data_provider) === []
     end
 
-    test "test result (page size: 1, page: 0)", %{data_list: data_list, data_provider: data_provider} do
-      data_provider = load_state(data_provider, 1, 0)
+    test "test result (page: 0, page size: 1)", %{data_list: data_list} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 1}
+        }
+      }
 
       assert ListModifier.modify(data_list, data_provider) === []
     end
 
-    test "test result (page size: 13, page: 13)", %{data_list: data_list, data_provider: data_provider} do
-      data_provider = load_state(data_provider, 13, 13)
+    test "test result (page: 13, page size: 13)", %{data_list: data_list} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 13,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
 
       assert ListModifier.modify(data_list, data_provider) === Enum.to_list(157..169)
     end
--- test/data_provider/data/query_modifier_test.exs
+++ test/data_provider/data/query_modifier_test.exs
@@ -5,11 +5,8 @@ defmodule DataProvider.Data.QueryModifierTest do
   alias DataProvider.Data.QueryModifier
   import Ecto.Query
 
-  def fixture(:data_provider, _params), do: DataProvider.create()
   def fixture(:query, _params), do: from(t in TestSchema)
 
-
-  defp data_provider(params), do: {:ok, data_provider: fixture(:data_provider, params)}
 
   defp query(params), do: {:ok, query: fixture(:query, params)}
 
@@ -21,66 +18,89 @@ defmodule DataProvider.Data.QueryModifierTest do
     |> elem(0)
   end
 
-  defp set_pagination_state(data_provider, page_size, page) do
-    pagination = %{data_provider.pagination | page_size: page_size, page: page}
-    %{data_provider | pagination: pagination}
-  end
-
-  defp set_sorting_state(data_provider, params), do: %{data_provider | sort: %{data_provider.sort | options: params}}
-
-  setup [:data_provider, :query]
+  setup [:query]
 
   describe "`modify/2` |" do
-    test "test offset and limit (page size: 0, page: 0)", %{query: query, data_provider: data_provider} do
-      data_provider = set_pagination_state(data_provider, 0, 0)
+    test "test offset and limit (page: 0, page size: 0)", %{query: query} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
       new_query = QueryModifier.modify(query, data_provider)
 
-      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.page_size
+      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.params.page_size
       assert get_sort_limit_expr(new_query, :offset) === 0
     end
 
-    test "test offset and limit (page size: 0, page: 1)", %{query: query, data_provider: data_provider} do
-      data_provider = set_pagination_state(data_provider, 0, 1)
+    test "test offset and limit (page: 1, page size: 0)", %{query: query} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 1,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
       new_query = QueryModifier.modify(query, data_provider)
 
-      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.page_size
+      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.params.page_size
       assert get_sort_limit_expr(new_query, :offset) === 0
     end
 
-    test "test offset and limit (page size: 0, page: 13)", %{query: query, data_provider: data_provider} do
-      data_provider = set_pagination_state(data_provider, 0, 13)
+    test "test offset and limit (page: 13, page size: 0)", %{query: query} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 13,
+          params: %DataProvider.Pagination.Params{page_size: 0}
+        }
+      }
       new_query = QueryModifier.modify(query, data_provider)
 
-      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.page_size
+      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.params.page_size
       assert get_sort_limit_expr(new_query, :offset) === 0
     end
 
-    test "test offset and limit (page size: 13, page: 0)", %{query: query, data_provider: data_provider} do
-      data_provider = set_pagination_state(data_provider, 13, 0)
+    test "test offset and limit (page: 0, page size: 13)", %{query: query} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
       new_query = QueryModifier.modify(query, data_provider)
 
-      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.page_size
+      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.params.page_size
       assert get_sort_limit_expr(new_query, :offset) === 0
     end
 
-    test "test offset and limit (page size: 1, page: 0)", %{query: query, data_provider: data_provider} do
-      data_provider = set_pagination_state(data_provider, 1, 0)
+    test "test offset and limit (page: 0, page size: 1)", %{query: query} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 0,
+          params: %DataProvider.Pagination.Params{page_size: 1}
+        }
+      }
       new_query = QueryModifier.modify(query, data_provider)
 
-      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.page_size
+      assert get_sort_limit_expr(new_query, :limit) === data_provider.pagination.params.page_size
       assert get_sort_limit_expr(new_query, :offset) === 0
     end
 
-    test "test offset and limit (page size: 13, page: 13)", %{query: query, data_provider: data_provider} do
-      data_provider = set_pagination_state(data_provider, 13, 13)
+    test "test offset and limit (page: 13, page size: 13)", %{query: query} do
+      data_provider = %DataProvider{
+        pagination: %DataProvider.Pagination{
+          page: 13,
+          params: %DataProvider.Pagination.Params{page_size: 13}
+        }
+      }
       new_query = QueryModifier.modify(query, data_provider)
 
       assert get_sort_limit_expr(new_query, :limit) === 13
       assert get_sort_limit_expr(new_query, :offset) === (13 - 1) * 13
     end
 
-    test "test order expression", %{query: query, data_provider: data_provider} do
-      data_provider = set_sorting_state(data_provider, desc: :inserted_at)
+    test "test order expression", %{query: query} do
+      data_provider = %DataProvider{sort: %DataProvider.Sort{options: [desc: :inserted_at]}}
       new_query = QueryModifier.modify(query, data_provider)
 
       assert new_query.order_bys |> Enum.count() > 0
