75bc6677ad68cb5ecae53864919ef0e86e704873
Shakarim Sapa <info@relitech.kz>
2021-10-14 23:48:59 +0600

    full rework `DataProvider.PaginationTest` logic

--- test/data_provider/pagination_test.exs
+++ test/data_provider/pagination_test.exs
@@ -3,137 +3,134 @@ defmodule DataProvider.PaginationTest do
   alias DataProvider.Pagination
   doctest DataProvider.Pagination
 
-  def fixture(:pagination, _params), do: Pagination.create()
-
-  defp pagination(params), do: {:ok, pagination: fixture(:pagination, params)}
-
-  defp load_state(pagination, page_size, page), do: %{pagination | page_size: page_size, page: page}
-
-  setup [:pagination]
-
   describe "`create/1` |" do
     test "test valid attrs" do
-      pagination = Pagination.create(%{page: 5, page_size: 100})
+      result = Pagination.create(%{page: 5, params: %{page_size: 100}})
 
-      assert match?(%Pagination{}, pagination)
-      assert pagination.page === 5
-      assert pagination.page_size === 100
+      assert result === %Pagination{page: 5, params: %Pagination.Params{page_size: 100}}
     end
 
     test "test default attrs" do
-      pagination = Pagination.create(%{})
+      result = Pagination.create(%{})
 
-      assert match?(%Pagination{}, pagination)
-      assert is_integer(pagination.page)
-      assert is_integer(pagination.page_size)
+      assert result === %Pagination{page: 1, params: %Pagination.Params{page_size: 15}}
     end
   end
 
-  describe "`start_position/1` |" do
-    test "test result (page size: 0, page: 0)", %{pagination: pagination} do
-      pagination = load_state(pagination, 0, 0)
-
-      assert Pagination.start_position(pagination) === 0
-    end
-
-    test "test result (page size: 0, page: 1)", %{pagination: pagination} do
-      pagination = load_state(pagination, 0, 1)
-
-      assert Pagination.start_position(pagination) === 0
-    end
-
-    test "test result (page size: 0, page: 13)", %{pagination: pagination} do
-      pagination = load_state(pagination, 0, 13)
-
-      assert Pagination.start_position(pagination) === 0
-    end
-
-    test "test result (page size: 13, page: 0)", %{pagination: pagination} do
-      pagination = load_state(pagination, 13, 0)
-
-      assert Pagination.start_position(pagination) === 0
-    end
-
-    test "test result (page size: 13, page: 1)", %{pagination: pagination} do
-      pagination = load_state(pagination, 13, 1)
-
-      assert Pagination.start_position(pagination) === 0
-    end
-
-    test "test result (page size: 13, page: 13)", %{pagination: pagination} do
-      pagination = load_state(pagination, 13, 13)
-
-      assert Pagination.start_position(pagination) === (13 - 1) * 13
-    end
-  end
-
-  describe "`end_position/1` |" do
-    test "test result (page size: 0, page: 0)", %{pagination: pagination} do
-      pagination = load_state(pagination, 0, 0)
-
-      assert Pagination.end_position(pagination) === 0
-    end
-
-    test "test result (page size: 0, page: 1)", %{pagination: pagination} do
-      pagination = load_state(pagination, 0, 1)
-
-      assert Pagination.end_position(pagination) === 0
-    end
-
-    test "test result (page size: 0, page: 13)", %{pagination: pagination} do
-      pagination = load_state(pagination, 0, 13)
-
-      assert Pagination.end_position(pagination) === 0
-    end
-
-    test "test result (page size: 13, page: 0)", %{pagination: pagination} do
-      pagination = load_state(pagination, 13, 0)
-
-      assert Pagination.end_position(pagination) === 0
-    end
-
-    test "test result (page size: 13, page: 1)", %{pagination: pagination} do
-      pagination = load_state(pagination, 13, 1)
-
-      assert Pagination.end_position(pagination) === (13 * 1) - 1
-    end
-
-    test "test result (page size: 13, page: 13)", %{pagination: pagination} do
-      pagination = load_state(pagination, 13, 13)
-
-      assert Pagination.end_position(pagination) === (13 * 13) - 1
-    end
-  end
-
-  test "`selection_limit/1` |", %{pagination: pagination} do
-    result = Pagination.selection_limit(pagination)
-
-    assert is_integer(result)
-  end
-
-  test "`page/1` |", %{pagination: pagination} do
+  test "`page/1` |" do
+    pagination = %Pagination{page: 5}
     result = Pagination.page(pagination)
 
-    assert is_integer(result)
+    assert result === 5
   end
 
-  test "`page_size/1` |", %{pagination: pagination} do
-    result = Pagination.page_size(pagination)
-
-    assert is_integer(result)
-  end
-
-  test "`put_page/1` |", %{pagination: pagination} do
+  test "`put_page/1` |" do
+    pagination = %Pagination{page: 5, params: %Pagination.Params{page_size: 100}}
     result = Pagination.put_page(pagination, 10)
 
-    assert result.page !== pagination.page
+    assert pagination.page === 5
     assert result.page === 10
   end
 
-  test "`put_page_size/1` |", %{pagination: pagination} do
-    result = Pagination.put_page_size(pagination, 100)
+  test "`put_params/2 |" do
+    pagination = %Pagination{}
+    result = Pagination.put_params(pagination, %{page_size: 100})
 
-    assert result.page_size !== pagination.page_size
-    assert result.page_size === 100
+    assert result === %Pagination{page: 1, params: %Pagination.Params{page_size: 100}}
+  end
+
+  describe "`start_position/1` |" do
+    test "test result (page: 0, page size: 0)" do
+      pagination = %Pagination{page: 0, params: %Pagination.Params{page_size: 0}}
+      result = Pagination.start_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 1, page size: 0)" do
+      pagination = %Pagination{page: 1, params: %Pagination.Params{page_size: 0}}
+      result = Pagination.start_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 13, page size: 0)" do
+      pagination = %Pagination{page: 13, params: %Pagination.Params{page_size: 0}}
+      result = Pagination.start_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 0, page size: 13)" do
+      pagination = %Pagination{page: 0, params: %Pagination.Params{page_size: 13}}
+      result = Pagination.start_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 1, page size: 13)" do
+      pagination = %Pagination{page: 1, params: %Pagination.Params{page_size: 13}}
+      result = Pagination.start_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 13, page size: 13)" do
+      pagination = %Pagination{page: 13, params: %Pagination.Params{page_size: 13}}
+      result = Pagination.start_position(pagination)
+
+      assert result === (13 - 1) * 13
+    end
+  end
+
+  test "`selection_limit/1` |" do
+    pagination = %Pagination{page: 0, params: %Pagination.Params{page_size: 25}}
+    result = Pagination.selection_limit(pagination)
+
+    assert result === 25
+  end
+
+  describe "`end_position/1` |" do
+    test "test result (page: 0, page size: 0)" do
+      pagination = %Pagination{page: 0, params: %Pagination.Params{page_size: 0}}
+      result = Pagination.end_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 1, page size: 0)" do
+      pagination = %Pagination{page: 1, params: %Pagination.Params{page_size: 0}}
+      result = Pagination.end_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 13, page size: 0)" do
+      pagination = %Pagination{page: 13, params: %Pagination.Params{page_size: 0}}
+      result = Pagination.end_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 0, page size: 13)" do
+      pagination = %Pagination{page: 0, params: %Pagination.Params{page_size: 13}}
+      result = Pagination.end_position(pagination)
+
+      assert result === 0
+    end
+
+    test "test result (page: 1, page size: 13)" do
+      pagination = %Pagination{page: 1, params: %Pagination.Params{page_size: 13}}
+      result = Pagination.end_position(pagination)
+
+      assert result === (13 * 1) - 1
+    end
+
+    test "test result (page: 13, page size: 13)" do
+      pagination = %Pagination{page: 13, params: %Pagination.Params{page_size: 13}}
+      result = Pagination.end_position(pagination)
+
+      assert result === (13 * 13) - 1
+    end
   end
 end
